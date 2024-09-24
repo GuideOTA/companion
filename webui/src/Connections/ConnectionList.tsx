5b864c650e8d1e8b31e33039989abba3fdb09a03
Julian Waller <git@julusian.co.uk>
2024-09-24 22:27:42 +0100

    wip: attempt at ui for adding modules

--- webui/src/Connections/ConnectionList.tsx
+++ webui/src/Connections/ConnectionList.tsx
@@ -307,7 +307,7 @@ const ConnectionsTableRow = observer(function ConnectionsTableRow({
 			<td onClick={doEdit} className="hand">
 				{moduleInfo ? (
 					<>
-						{moduleInfo.selectedVersion.isLegacy && (
+						{moduleInfo.defaultVersion.isLegacy && (
 							<>
 								<FontAwesomeIcon
 									icon={faExclamationTriangle}
